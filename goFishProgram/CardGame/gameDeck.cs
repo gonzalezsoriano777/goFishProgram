b13a1b570ff6c207b3f7111e1b1fd4fbfd30e1fb
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-12-18 17:48:15 -0600

    gameDeck file is complete. game functionality is commencing.

--- goFishProgram/CardGame/gameDeck.cs
+++ goFishProgram/CardGame/gameDeck.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace goFishProgram
 { 
-    public enum cardFaceValues { Ace = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, King = 12, Queen = 13 };
+    public enum cardFaceValues {Ace = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack = 11, King = 12, Queen = 13};
     public enum cardSuits { Dolp, Sharks, Orcas, Meg };
 
     public class gameDeck
